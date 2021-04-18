a863c17002500daf9df2c65c257aa053a5fb409b
matthew <mematthew123@gmail.com>
2021-04-17 21:16:37 -0600

    added account holder to Merit Bank Class

--- src/main/java/com/meritamerica/assignment5/controller/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment5/controller/MeritAmericaBankApp.java
@@ -1,12 +1,20 @@
 package com.meritamerica.assignment5.controller;
 
-import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.meritamerica.assignment5.models.AccountHolder;
+import com.meritamerica.assignment5.models.MeritBank;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+
 
 @RestController
 public class MeritAmericaBankApp {
@@ -16,8 +24,36 @@ public class MeritAmericaBankApp {
 	
 	//accountHolders.add(accHolder);
 	
-	@GetMapping("/accountHolders")
-	public AccountHolder home() {
-		return accHolder2;
+//	@GetMapping("/accountHolders")
+//	public AccountHolder home() {
+//		return accHolder2;
+//	}
+
+	@PostMapping(value = "/AccountHolder")
+	
+	public AccountHolder addAccountHolder(@RequestBody  AccountHolder accountHolder) {
+		MeritBank.addAccountHolder(accountHolder);
+		return accountHolder;
 	}
+	// TODO complete
+	@GetMapping(value = "/AccountHolder")
+	public AccountHolder[] getListOfAccountHolders() {
+		return MeritBank.getAccountHolders();
+	}
+	// TODO complete
+	@GetMapping(value = "/AccountHolder/{id}")
+	public AccountHolder getAccountHolderById(@PathVariable("id") long id) throws Exception {
+		AccountHolder accountHolder = MeritBank.addAccountHolder(id);
+		if(accountHolder == null) { 
+		
+			throw new Exception("Account Not Found"); 
+		}
+		return accountHolder;
+	}
+
+
+	
+	
+	
+	
 }
--- src/main/java/com/meritamerica/assignment5/models/MeritBank.java
+++ src/main/java/com/meritamerica/assignment5/models/MeritBank.java
@@ -1,5 +1,39 @@
 package com.meritamerica.assignment5.models;
 
+import javax.validation.Valid;
+
 public class MeritBank {
 
+	public static void addAccountHolder(@Valid AccountHolder accountHolder) {
+		// TODO Auto-generated method stub
+		AccountHolder[] newAccountHolders = new AccountHolder[accountHolders.length + 1];
+		int i = 0;
+		for (i = 0; i < accountHolders.length; i++) {
+			newAccountHolders[i] = accountHolders[i];
+		}
+		newAccountHolders[i] = accountHolder;
+		accountHolders = newAccountHolders;
+	
+	}
+
+	public static AccountHolder[] getAccountHolders() {
+		// TODO Auto-generated method stub
+		return accountHolders;
+		
+		
+	}
+
+	public static AccountHolder addAccountHolder(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private static AccountHolder[] accountHolders = new AccountHolder[0];
+	private static CDOffering[] cdOfferings = new CDOffering[0];
+
+
+
+
+	
+	
+	
 }
